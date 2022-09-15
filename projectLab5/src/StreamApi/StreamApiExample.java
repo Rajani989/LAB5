13e12007a5c909bf62c4dcf44fb2475004fcd38e
Rajani <Rajani@admin>
2022-09-15 17:43:38 +0530

    commit

--- projectLab5/src/StreamApi/StreamApiExample.java
+++ projectLab5/src/StreamApi/StreamApiExample.java
@@ -7,20 +7,7 @@ package StreamApi;
 
 import java.util.*;
 import java.util.stream.Collectors;
-class student            //class student
-{
-	int id;
-	String name;
-	int age;
-    double marks;
-	public student(int id,String name,int age, double marks)   //constructor of student
-	{
-		this.id=id;
-		this.name=name;
-		this.age=age;
-		this.marks=marks;
-	}
-}
+
 
 public class StreamApiExample {                             //class streamSpiExample
 
---
+++ projectLab5/src/StreamApi/student.java
@@ -0,0 +1,16 @@
+package StreamApi;
+
+public class student            //class student
+{
+	int id;
+	String name;
+	int age;
+    double marks;
+	public student(int id,String name,int age, double marks)   //constructor of student
+	{
+		this.id=id;
+		this.name=name;
+		this.age=age;
+		this.marks=marks;
+	}
+}
